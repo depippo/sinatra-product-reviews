775dbaa1964ad7c00922a69243bf0541be4921b8
depippo <jdepippo@gmail.com>
2016-06-03 15:21:58 -0400

    Added product views

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,4 +1,13 @@
 class ProductController < ApplicationController
 
+  get '/products' do
+    @products = Product.all
+    erb :'/products/index'
+  end
+
+  get '/products/:slug' do
+    @product = Product.find_by_slug(params[:slug])
+    erb :'/products/show_product'
+  end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product <ActiveRecord::Base
   has_many :reviews
+
+  def slug
+    self.name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |product|
+      product.slug == slug
+    end
+  end
+
 end
