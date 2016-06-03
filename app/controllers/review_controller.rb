010f364be2927b02dbf939b098ab9afc6e741d53
depippo <jdepippo@gmail.com>
2016-06-03 14:45:59 -0400

    Added edit review function

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -39,6 +39,29 @@ class ReviewController < ApplicationController
     end
   end
 
+  get '/reviews/:id/edit' do
+    if Helpers.is_logged_in?(session)
+      @review = Review.find(params[:id])
+      if @review.user_id == Helpers.current_user(session).id
+        erb :'/reviews/edit_review'
+      else
+        redirect to "/reviews"
+      end
+    else
+      redirect to "/login"
+    end
+  end
+
+  patch '/reviews/:id' do
+    @review = Review.find(params[:id])
+    if params[:content] != ""
+      @review.update(content: params[:content])
+      redirect to "/reviews"
+    else
+      redirect to "/reviews/#{@review.id}/edit"
+    end
+  end
+
   delete '/reviews/:id/delete' do
     @review = Review.find(params[:id])
     if @review.user_id == Helpers.current_user(session).id
